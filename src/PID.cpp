0c7827112799909d03ecc14556f364566c693d0b
YUC1FH <chuan.yu@us.bosch.com>
2018-11-05 20:24:12 -0500

    Commit initial files for PID project

---
+++ src/PID.cpp
@@ -0,0 +1,34 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_in, double Ki_in, double Kd_in) {
+    Kp = Kp_in;
+    Ki = Ki_in;
+    Kd = Kd_in;
+    
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+}
+
+double PID::TotalError() {
+    double steer_value;
+    steer_value = - Kp * p_error - Ki * i_error - Kd * d_error;
+    return steer_value;
+}
+
